8ed3c01e7fccbf61b9526b6fe95147f95d5bf011
peta swan <petaswan986@gmail.com>
2023-09-21 15:18:43 +1000

    HTML, CSS & JS code updates

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -19,8 +19,63 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+  
+  
+  $(document).ready(function() {
+
+      // TODO: Add code to display the current date in the header of the page.
   var today = dayjs();
-$('#currentDay').text(today.format('dddd, MMMM Do'));
+$('#currentDay').text(today.format('dddd, MMMM DD'));
+
+function timeTracker() {
+  //get current number of hours.
+  var timeNow = dayjs().format("HH");
+  console.log(timeNow);
+
+  // loop over time blocks
+  $(".time-block").each(function () {
+      var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+      // To check the time and add the classes for background indicators
+      if (blockTime < timeNow) {
+          $(this).removeClass("future");
+          $(this).removeClass("present");
+          $(this).addClass("past");
+      }
+      else if (blockTime === timeNow) {
+          $(this).removeClass("past");
+          $(this).removeClass("future");
+          $(this).addClass("present");
+      }
+      else {
+          $(this).removeClass("present");
+          $(this).removeClass("past");
+          $(this).addClass("future");
+
+      }
+  })
+}
+
+//grabs values from time and value divs and saves them to local storage
+$(".saveBtn").click (function (event) {
+  event.preventDefault();
+  var text = $(this).siblings(".").val();
+  var time = $(this).parent().attr("id");
+  localStorage.setItem(time, text);
+})
+
+//retrieves items from local storage and sets them in proper places
+$("#hour-09 .description").val(localStorage.getItem("09"));
+$("#hour-10 .description").val(localStorage.getItem("10"));
+$("#hour-11 .description").val(localStorage.getItem("11"));
+$("#hour-12 .description").val(localStorage.getItem("12"));
+$("#hour-13 .description").val(localStorage.getItem("13"));
+$("#hour-14 .description").val(localStorage.getItem("14"));
+$("#hour-15 .description").val(localStorage.getItem("15"));
+$("#hour-16 .description").val(localStorage.getItem("16"));
+$("#hour-17 .description").val(localStorage.getItem("17"));
+
+timeTracker();
 });
-//ADD PLUG IN FOR 'Do'
+
+
